15fd5dc1c5511071fe81644a36d4d8f144efa240
NORDNETDOM\gyebadokpo.ext <Obed1990>
2017-03-24 14:11:01 +0100

    First Commit

---
+++ src/main/java/com/example/oluniyin/SecuritycustomApplication3.java
@@ -0,0 +1,12 @@
+package com.example.oluniyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritycustomApplication3 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritycustomApplication3.class, args);
+	}
+}
